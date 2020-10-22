e1b4945ee6ba915993cbcbe318b56151d2db1692
Chuck Dries <chuck@chuckdries.com>
2020-10-21 20:28:32 -0700

    fix building

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,7 +8,7 @@
 export const onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
   if (typeof window.IntersectionObserver === `undefined`) {
-    import(`intersection-observer`);
+    import(`intersection-observer`); // eslint-disable-line
     console.log(`# IntersectionObserver is polyfilled!`);
   }
 };
---
+++ src/pages/prose.jsx
@@ -0,0 +1,45 @@
+
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { graphql, Link } from "gatsby"
+
+const PostsList = props => {
+  const { edges: posts } = props.data.allMdx
+  console.log("post props", posts)
+  return (
+    <Layout>
+      <SEO title="All Posts" />
+      <ul>
+        {posts.map(({node}) => (
+          <div>
+            <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </ul>
+      <p>hello world</p>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query ProseList {
+    allMdx(filter: {fields: {slug: {regex: "^/prose/.+/"}}}) {
+      edges {
+        node {
+          excerpt
+          frontmatter {
+            title
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
+
+export default PostsList
